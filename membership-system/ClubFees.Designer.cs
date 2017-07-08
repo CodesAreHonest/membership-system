0460e54d06c6e54f2f90ccbf7d4c229aa29b744a
YingHua <yinghuachai0@gmail.com>
2017-07-08 14:45:56 +0800

    clubfees ui restructure

--- membership-system/ClubFees.Designer.cs
+++ membership-system/ClubFees.Designer.cs
@@ -33,10 +33,7 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.paidTextbox = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.haventPaidTextbox = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.registerStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -53,7 +50,7 @@
             this.registerStrip.Name = "registerStrip";
             this.registerStrip.Padding = new System.Windows.Forms.Padding(20, 0, 1, 0);
             this.registerStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
-            this.registerStrip.Size = new System.Drawing.Size(788, 43);
+            this.registerStrip.Size = new System.Drawing.Size(1185, 43);
             this.registerStrip.TabIndex = 6;
             this.registerStrip.Text = "toolStrip1";
             // 
@@ -70,79 +67,47 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 153);
+            this.dataGridView1.Location = new System.Drawing.Point(476, 116);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(763, 362);
+            this.dataGridView1.Size = new System.Drawing.Size(697, 362);
             this.dataGridView1.TabIndex = 10;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(29, 64);
+            this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(550, 70);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(195, 18);
+            this.label1.Size = new System.Drawing.Size(116, 17);
             this.label1.TabIndex = 11;
-            this.label1.Text = "Search Member who paid :";
+            this.label1.Text = "Search Member:";
             // 
             // paidTextbox
             // 
             this.paidTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.paidTextbox.Location = new System.Drawing.Point(291, 60);
+            this.paidTextbox.Location = new System.Drawing.Point(710, 66);
             this.paidTextbox.Name = "paidTextbox";
             this.paidTextbox.Size = new System.Drawing.Size(257, 24);
             this.paidTextbox.TabIndex = 12;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(29, 100);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(252, 18);
-            this.label2.TabIndex = 13;
-            this.label2.Text = "Search Member who havent \'paid :";
-            // 
-            // haventPaidTextbox
-            // 
-            this.haventPaidTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.haventPaidTextbox.Location = new System.Drawing.Point(291, 100);
-            this.haventPaidTextbox.Name = "haventPaidTextbox";
-            this.haventPaidTextbox.Size = new System.Drawing.Size(257, 24);
-            this.haventPaidTextbox.TabIndex = 14;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.LightSkyBlue;
             this.button1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(575, 58);
+            this.button1.Location = new System.Drawing.Point(993, 64);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(127, 30);
             this.button1.TabIndex = 15;
             this.button1.Text = "Search ";
             this.button1.UseVisualStyleBackColor = false;
             // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.button2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(575, 100);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(127, 30);
-            this.button2.TabIndex = 16;
-            this.button2.Text = "Search ";
-            this.button2.UseVisualStyleBackColor = false;
-            // 
             // ClubFees
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(788, 527);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(1185, 527);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.haventPaidTextbox);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.paidTextbox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
@@ -165,9 +130,6 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox paidTextbox;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox haventPaidTextbox;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
