60b5215a9daaf6960e2890701b3745f69a6f6ea0
YingHua <yinghuachai0@gmail.com>
2017-07-16 21:15:21 +0800

    update feature done

--- membership-system/MeetingDashboard.cs
+++ membership-system/MeetingDashboard.cs
@@ -180,7 +180,50 @@ namespace membership_system
 
         private void updateButton_Click(object sender, EventArgs e)
         {
-            
+            validateEmptyField();
+            if (insertMeetingData)
+            {
+                Meeting m = new Meeting(session);
+                m.setName(nameTextbox.Text);
+                m.setLocation(locationTextbox.Text);
+                m.setStartTime(datePicker.Value, startTimePicker.Value);
+                m.setEndTime(datePicker.Value, endTimePicker.Value);
+                m.setDescription(descriptionTextbox.Text);
+                m.calculateDuration();
+
+                Club club = new Club();
+
+                SqlConn connect = new SqlConn();
+                connect.open();
+                SqlCommand command = new SqlCommand();
+                command.Connection = connect.sqlConnection;
+                command.CommandText = "update dbo.meeting set meeting_name = '" + m.getMeetingName()
+                    + "', meeting_location = '" + m.getMeetingLocation()
+                    + "', meeting_description = '" + m.getMeetingDescription()
+                    + "', meeting_starttime = '" + m.getStartTime()
+                    + "', meeting_endtime = '" + m.getEndTime()
+                    + "', meeting_duration = " + m.getDuration() +
+                    "where meeting_id = " + m.getMeetingID() + "and club_id = " + club.getClubIDFromPresident(session);
+
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Close();
+                connect.close();
+
+                // success message
+                messageText.ForeColor = System.Drawing.Color.Green;
+                messageText.Text = "Update meeting details successful. ";
+
+                displayAllField();
+                clearField();
+            }
+            else
+            {
+                MessageBox.Show("Your UPDATE action is INVALID because some field is empty or wrong");
+
+                // warning message
+                messageText.ForeColor = System.Drawing.Color.Red;
+                messageText.Text = "UPDATE action is INVALID \n because some field is empty or wrong ! ";
+            }
         }
 
         private void meetingGridView_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
@@ -190,7 +233,7 @@ namespace membership_system
             descriptionTextbox.Text = meetingGridView.Rows[e.RowIndex].Cells[2].Value.ToString();
             datePicker.Text = meetingGridView.Rows[e.RowIndex].Cells[3].Value.ToString();
             startTimePicker.Text = meetingGridView.Rows[e.RowIndex].Cells[3].Value.ToString();
-            endTimePicker.Text = meetingGridView.Rows[e.RowIndex].Cells[3].Value.ToString();
+            endTimePicker.Text = meetingGridView.Rows[e.RowIndex].Cells[4].Value.ToString();
 
             Meeting m = new Meeting(session);
             m.setName(nameTextbox.Text);
