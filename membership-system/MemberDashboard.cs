9a1e4dbed7a4186c810327d5958c713d81ceecfc
YingHua <yinghuachai0@gmail.com>
2017-07-19 17:34:09 +0800

    fix affected area: - club fees - club registration - member dashboard

--- membership-system/MemberDashboard.cs
+++ membership-system/MemberDashboard.cs
@@ -287,5 +287,15 @@ namespace membership_system
             reader.Close();
             connect.close();
         }
+
+        private void studenthpTextbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            if (!char.IsDigit(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+            }
+        }
     }
 }
